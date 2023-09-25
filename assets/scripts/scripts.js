5a3b469958975bac25539a2b41c5f08da8ce1e2d
Lucas Garcia Buixens <Lucasgb@hotmail.com>
2023-09-25 14:45:16 -0300

    agregado captcha y mejoras en info

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -223,4 +223,67 @@ AOS.init();
         // },
       });
 
-    
+    // captcha con JS //-----------------------------------------------
+
+    let respuestaCorrecta;
+
+        function generarOperacion() {
+            const num1 = Math.floor(Math.random() * 10) + 1; // Número entero positivo entre 1 y 10
+            const num2 = Math.floor(Math.random() * 10) + 1; // Número entero positivo entre 1 y 10
+            const operaciones = ['+', '*', '-'];
+            const operacion = operaciones[Math.floor(Math.random() * operaciones.length)];
+
+            let resultado;
+            switch (operacion) {
+                case '+':
+                    resultado = num1 + num2;
+                    break;
+                case '*':
+                    resultado = num1 * num2;
+                    break;
+                case '-':
+                    resultado = num1 - num2;
+                    break;
+            }
+
+            respuestaCorrecta = resultado;
+            return `${num1} ${operacion} ${num2}`;
+        }
+
+        function reiniciarCaptcha() {
+            const pregunta = generarOperacion();
+            document.getElementById('recaptcha').innerHTML = pregunta;
+            document.getElementById('respuesta').value = ''; // Clear the input field
+        }
+
+        function validarRespuesta() {
+            const respuestaUsuario = parseInt(document.getElementById('respuesta').value);
+            const enviarButton = document.getElementById('enviar');
+            enviarButton.disabled = isNaN(respuestaUsuario) || respuestaUsuario !== respuestaCorrecta;
+            // document.getElementById('mensajeCaptcha').innerText = 'Por favor, ingresa una respuesta válida.';
+        }
+
+        // function validarFormulario() {
+        //     const respuestaUsuario = parseInt(document.getElementById('respuesta').value);
+        //     if (isNaN(respuestaUsuario)) {
+        //         document.getElementById('mensajeCaptcha').innerText = 'Por favor, ingresa una respuesta válida.';
+        //         return false;
+        //     }
+
+        //     if (respuestaUsuario === respuestaCorrecta) {
+        //         document.getElementById('mensajeCaptcha').innerText = '';
+        //         alert('Enviado');
+        //         return true; // Permite el envío del formulario
+        //     } else {
+        //         document.getElementById('mensajeCaptcha').innerText = 'Captcha incorrecto.';
+        //         reiniciarCaptcha(); // Generate a new operation and clear the input field
+        //         return false; // Evitar el envío del formulario
+        //     }
+        // }
+
+        window.onload = function() {
+            reiniciarCaptcha();
+            const respuestaInput = document.getElementById('respuesta');
+            respuestaInput.addEventListener('input', validarRespuesta); // Add event listener for input changes in the answer
+        };
+  
